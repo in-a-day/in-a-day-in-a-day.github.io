e72f74aaf9e6ac31b5bffe35ccf2756ced1190dc
nichijou <zhangbshu@gmail.com>
2021-09-23 23:59:29 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ githubUrl=https://github.com/in-a-day/in-a-day.github.io.git
 git init
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
+git push -f $githubUrl main:gh-pages # 推送到github gh-pages分支
 
 # deploy to coding pages
 # echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
