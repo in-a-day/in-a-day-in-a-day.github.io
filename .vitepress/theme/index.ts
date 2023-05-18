43fa9ae3011b56dfd4ab5e00aa8e2d4f634639d1
zabbits <zhangbshu@gmail.com>
2023-05-18 13:12:10 +0800

    img zoom

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,3 +1,6 @@
+import { EnhanceAppContext, inBrowser, useRouter } from 'vitepress'
+import { nextTick, watchEffect } from 'vue'
+import mediumZoom from 'medium-zoom'
 import DefaultTheme from "vitepress/theme";
 import Archives from "./components/Archives.vue";
 import Tags from "./components/Tags.vue";
@@ -7,11 +10,19 @@ import "./custom.css";
 export default {
   ...DefaultTheme,
   Layout: MyLayout,
-  enhanceApp(ctx: any) {
+  enhanceApp(ctx: EnhanceAppContext) {
     DefaultTheme.enhanceApp(ctx);
     const { app } = ctx;
     // register global components
     app.component("Archives", Archives);
     app.component("Tags", Tags);
   },
+  setup() {
+    const router = useRouter()
+    watchEffect(() => {
+      // 将router.route.path作为依赖收集 首次访问即添加监听
+      const path = router.route.path
+      nextTick(() => (inBrowser ? mediumZoom('.main img', { background: 'var(--vp-c-bg)' }) : null))
+    })
+  }
 };
